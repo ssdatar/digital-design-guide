78a41be5c17293e7f55592d7e1e279c28b56500c
Ashlyn Still <ashlyn.still@gmail.com>
2015-07-28 15:14:50 -0400

    move from src to root

--- scripts/main.js
+++ scripts/main.js
@@ -17,7 +17,7 @@
 		 	'colorID': 'green'
 		},
 		{
-		 	'color': '#004181',
+		 	'color': '#3e7b9b',
 		 	'colorID': 'blue'
 		},
 		{
@@ -30,7 +30,7 @@
 		}  
 	];
 
-//a comment
+
 	var darkShade, lightShade, shadeScale;
 
 
@@ -38,12 +38,11 @@
 
 		darkShade = chroma(color).saturate(),
 		lightShade = darkShade.brighten(50),
-		shadeScale = chroma.scale([darkShade, lightShade]).domain([0,7]).out('hex');
+		shadeScale = chroma.scale([darkShade, lightShade]).domain([0,6]).out('hex');
 		
 		$('#data-viz li').each(function(i){
 			$(scaleDiv).append('<span></span>');
 			$(scaleDiv).children('span:last-child').css({ 'background': shadeScale.mode('lab')(i) });
-
 		});
 		baseDiv.css({ 'background-color': shadeScale.mode('lab')(0) }).children('p:last-child').empty().append(color);
 	
